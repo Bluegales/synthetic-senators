13f4d73a329f2c2841b81cdc66d5ec67cd772ee2
lorbke <lorbke@student.42heilbronn.de>
2024-05-28 22:54:42 +0200

    add litAction to query Galadriel contract

--- lit/run.ts
+++ lit/run.ts
@@ -31,7 +31,7 @@ async function main() {
 
   const authSig = await getAuthSig(litNodeClient);
 
-  const litActionCode = `
+  const litActionProposalData = `
     const go = async () => {
       // get list of proposals
         const query = \`
@@ -97,12 +97,233 @@ async function main() {
     
     go();
   `;
+
+  const litActionGaladrielResponseData = `
+    const go = async () => {
+      const ABI = [
+          {
+            "inputs": [
+            {
+              "internalType": "address",
+              "name": "_oracleAddress",
+              "type": "address"
+            },
+            {
+              "internalType": "string",
+              "name": "_instruction",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "_name",
+              "type": "string"
+            }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+          },
+          {
+            "anonymous": false,
+            "inputs": [
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "newOracleAddress",
+              "type": "address"
+            }
+            ],
+            "name": "OracleAddressUpdated",
+            "type": "event"
+          },
+          {
+            "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+            ],
+            "name": "advises",
+            "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+          },
+          {
+            "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "proposalId",
+              "type": "uint256"
+            }
+            ],
+            "name": "getMessageHistoryContents",
+            "outputs": [
+            {
+              "internalType": "string[]",
+              "name": "",
+              "type": "string[]"
+            }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+          },
+          {
+            "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "proposalId",
+              "type": "uint256"
+            }
+            ],
+            "name": "getMessageHistoryRoles",
+            "outputs": [
+            {
+              "internalType": "string[]",
+              "name": "",
+              "type": "string[]"
+            }
+            ],
+            "stateMutability": "pure",
+            "type": "function"
+          },
+          {
+            "inputs": [
+            {
+              "internalType": "string",
+              "name": "proposal",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "proposalId",
+              "type": "uint256"
+            }
+            ],
+            "name": "getProposalAdvise",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+          },
+          {
+            "inputs": [],
+            "name": "instruction",
+            "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+          },
+          {
+            "inputs": [],
+            "name": "name",
+            "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+          },
+          {
+            "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "proposalId",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "response",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+            ],
+            "name": "onOracleLlmResponse",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+          },
+          {
+            "inputs": [],
+            "name": "oracleAddress",
+            "outputs": [
+            {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+            }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+          },
+          {
+            "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+            ],
+            "name": "proposals",
+            "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+          },
+          {
+            "inputs": [
+            {
+              "internalType": "address",
+              "name": "newOracleAddress",
+              "type": "address"
+            }
+            ],
+            "name": "setOracleAddress",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+          }
+      ]
+    
+      const providerUrl = 'https://devnet.galadriel.com'
+      const contractAddress = "0x91e2770cF1E511420AdfbdF668794910101Ecf90"
+    
+      const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+      const contract = new ethers.Contract(contractAddress, ABI, provider);
+    
+      const response = await contract.advises(0);
+      console.log(response);
+    }
+    
+    go();
+  `;
   // const pkpKey = ethers.BigNumber.from("0x04b9f18903a7ca5ad7e419f7553ef338f4cc3e2e86f31257cd54ad9ba03b02831421d51f873beb5dd92c68204ba0a7af9e65670e0023d3a075f8d67b025c7e65ae")
 
   // const pkpKey2 = ethers.BigNumber.from(pkpKey)
 
   const signatures = await litNodeClient.executeJs({
-    code: litActionCode,
+    code: litActionProposalData,
     authSig: authSig,
     jsParams: {
       toSign: [84, 104, 105, 115, 32, 109, 101, 115, 115, 97, 103, 101, 32, 105, 115, 32, 101, 120, 97, 99, 116, 108, 121, 32, 51, 50, 32, 98, 121, 116, 101, 115],
