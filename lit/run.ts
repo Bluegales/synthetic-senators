3794d3a7572ef06d52c09ed9d58045cc0eb0640e
lorbke <lorbke@student.42heilbronn.de>
2024-05-28 18:37:03 +0200

    add proposal data fetching

--- lit/run.ts
+++ lit/run.ts
@@ -32,23 +32,67 @@ async function main() {
   const authSig = await getAuthSig(litNodeClient);
 
   const litActionCode = `
-    const go = async () => {  
-      const url = "https://api.weather.gov/gridpoints/TOP/31,80/forecast";
-      const resp = await fetch(url).then((response) => response.json());
-      const temp = resp.properties.periods[0].temperature;
-    
-      // only sign if the temperature is above 60.  if it's below 60, exit.
-      if (temp < 60) {
-        LitActions.setResponse({ response: "temperature below 60" });
-        return;
-      }
-      
-      LitActions.setResponse({ response: "temperature above 60" });
+    const go = async () => {
+      // get list of proposals
+        const query = \`
+          query Proposals($chainId: ChainID!, $pagination: Pagination, $sort: ProposalSort) {
+          proposals(chainId: $chainId, pagination: $pagination, sort: $sort) {
+          id
+          title
+          description
+          eta
+          governor {
+            name
+          }
+          voteStats {
+            support
+            weight
+            votes
+            percent
+          }
+          }
+        }
+        \`;
+        const variables = {
+          chainId: "eip155:1",
+          pagination: { limit: 1, offset: 0 },
+          sort: { field: "START_BLOCK", order: "DESC" },
+        }; 
+        const url = 'https://api.tally.xyz/query';
+        const apiKey = 'd0c4916e4e60c95b3c77a22eb83e158638109a937c76210eafca951a3e950f5d'; // Replace with your actual API key
+        
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+          "Content-Type": "application/json",
+          'Api-Key': apiKey, // If an API key is required
+          },
+          body: JSON.stringify({
+          query: query,
+          variables: variables,
+          }),
+        });
+        
+        if (!response.ok) {
+          LitActions.setResponse({ response: "Network response was not ok" });
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        
+        const data = await response.json();
+        
+        if (data.errors) {
+          LitActions.setResponse({ response: "Error when fetching data"});
+          console.error('Error when fetching data:', data.errors);
+          return null;
+        }
+
+        console.log(data.data);
 
       // this requests a signature share from the Lit Node
       // the signature share will be automatically returned in the HTTP response from the node
       // all the params (toSign, publicKey, sigName) are passed in from the LitJsSdk.executeJs() function
       const sigShare = await LitActions.signEcdsa({ toSign, publicKey , sigName });
+      console.log("signing done")
     };
     
     go();
