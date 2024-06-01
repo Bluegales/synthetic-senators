bf25b4c8bb8a8fe8752c781f17b08b5b25b3dc9e
Patrick Fuchs <email@patrick-fuchs.com>
2024-06-01 21:55:53 +0200

    show multiple proposals add delegation

--- app/src/web3modalConfig.ts
+++ app/src/web3modalConfig.ts
@@ -6,13 +6,11 @@ import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
 import { QueryClient } from '@tanstack/react-query';
 import { mainnet, arbitrum, optimism, baseSepolia, sepolia} from 'wagmi/chains';
 
-
 export const queryClient = new QueryClient();
-
 export const projectId = '8a7f7aef4b5cfe2c2387efaea8d43f49';
 
 const metadata = {
-  name: 'DAOtik AIwar',
+  name: 'Synthetic Senators',
   description: 'DAO Voting made easy with AI agents',
   url: 'https://daotikaiwar.eth', 
   icons: ['https://daotikaiwar.eth/icon.png'],
